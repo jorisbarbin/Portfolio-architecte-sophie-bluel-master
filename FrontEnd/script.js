2c689f2e73a9befae369aaadb8f4605bfdc04340
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-15 17:08:00 +0200

    Dynamic gallery display

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,20 +1,19 @@
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then((data) => {
+        for (let i = 0; i < data.length; i++) {
         let figure = document.createElement("figure");
         let image = document.createElement("img");
-        image.setAttribute("src", data[0].imageUrl);
-        image.setAttribute("alt", data[0].title);
+        image.setAttribute("src", data[i].imageUrl);
+        image.setAttribute("alt", data[i].title);
         let figcaption = document.createElement("figcaption");
-        figcaption.textContent = data[0].title;
+        figcaption.textContent = data[i].title;
         figure.appendChild(image);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
-        console.log(figure);
-        console.log(image);
-        console.log(figcaption);
-        console.log("Ça fonctionne:", data[0]);
-    })
+
+        console.log("Ça fonctionne:", data[i]);
+    }})
     .catch((error) => {
         console.error("Ça ne fonctionne pas:", error);
     });
