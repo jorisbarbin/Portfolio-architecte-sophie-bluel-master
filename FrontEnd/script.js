bb6691ff7ccc3a77c8a451d79b9ed6e56d22585b
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-15 17:32:29 +0200

    add button filters from category + create button all

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,24 @@
 const gallery = document.querySelector(".gallery");
 gallery.innerHTML = "";
+const filters = document.querySelector(".filters");
+filters.innerHTML = "";
+
+fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+        let buttonAll = document.createElement("button");
+        buttonAll.textContent = "Tous";
+        buttonAll.setAttribute("category-data-id", "all");
+        filters.appendChild(buttonAll);
+        for (let i = 0; i < data.length; i++) {
+            let button = document.createElement("button");
+            button.textContent = data[i].name;
+            button.setAttribute("category-data-id", data[i].id);
+            filters.appendChild(button);
+        }
+        console.log("Categories ok:", data);
+    })
+
 
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
@@ -15,10 +34,3 @@ fetch("http://localhost:5678/api/works")
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
     }})
-
-
-
-
-
-
-
