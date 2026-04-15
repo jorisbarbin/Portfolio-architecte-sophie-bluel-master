7a96899adcaecb447e7286c77b38f0e18411e1c7
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-15 17:00:38 +0200

    Add first project from API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,19 @@
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then((data) => {
-        console.log("Ça fonctionne:", data);
+        let figure = document.createElement("figure");
+        let image = document.createElement("img");
+        image.setAttribute("src", data[0].imageUrl);
+        image.setAttribute("alt", data[0].title);
+        let figcaption = document.createElement("figcaption");
+        figcaption.textContent = data[0].title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+        console.log(figure);
+        console.log(image);
+        console.log(figcaption);
+        console.log("Ça fonctionne:", data[0]);
     })
     .catch((error) => {
         console.error("Ça ne fonctionne pas:", error);
@@ -12,3 +24,6 @@ const gallery = document.querySelector(".gallery");
 gallery.innerHTML = "";
 
 console.log(gallery);
+
+
+
