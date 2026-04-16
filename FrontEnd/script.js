650e05cbb8e498528469209241c7654f73a429be
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-16 13:18:47 +0200

    create function reusable for dysplaying gallery

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,47 +1,62 @@
-const gallery = document.querySelector(".gallery");
+let gallery = document.querySelector(".gallery");
 gallery.innerHTML = "";
 const filters = document.querySelector(".filters");
 filters.innerHTML = "";
+let dataWorks = [];
+
+
+fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((data) => {
+        dataWorks = data;
+        AfficherGallery(dataWorks);
+    })
+        function AfficherGallery(dataWorks) {
+        gallery.innerHTML = "";
+        for (let i = 0; i < dataWorks.length; i++) {
+        let figure = document.createElement("figure");
+        let image = document.createElement("img");
+        image.setAttribute("src", dataWorks[i].imageUrl);
+        image.setAttribute("alt", dataWorks[i].title);
+        let figcaption = document.createElement("figcaption");
+        figcaption.textContent = dataWorks[i].title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }}
+
+
 
 
 fetch("http://localhost:5678/api/categories")
     .then((response) => response.json())
     .then((data) => {
+        dataFilters = data;
 
         let buttonAll = document.createElement("button");
         buttonAll.textContent = "Tous";
         buttonAll.setAttribute("data-category-id", "all");
         filters.appendChild(buttonAll);
-        for (let i = 0; i < data.length; i++) {
+        for (let i = 0; i < dataFilters.length; i++) {
             let button = document.createElement("button");
-            button.textContent = data[i].name;
-            button.setAttribute("data-category-id", data[i].id);
+            button.textContent = dataFilters[i].name;
+            button.setAttribute("data-category-id", dataFilters[i].id);
             filters.appendChild(button);
             button.addEventListener("click", () => {
             console.log(button.getAttribute("data-category-id"))}
             )
         }
-            buttonAll.addEventListener("click", () => {
-            console.log(buttonAll.getAttribute("data-category-id"))}
+        buttonAll.addEventListener("click", () => {
+            
+            AfficherGallery(dataWorks);
+            
+            console.log(buttonAll.getAttribute("data-category-id"))
+            console.log(gallery.innerHTML)
+            console.log(AfficherGallery)
+        }
+            
             )
 
-
-
-        console.log("Categories ok:", data);        
+        console.log("Categories ok:", dataWorks);
 
     })
-
-fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) => {
-        for (let i = 0; i < data.length; i++) {
-        let figure = document.createElement("figure");
-        let image = document.createElement("img");
-        image.setAttribute("src", data[i].imageUrl);
-        image.setAttribute("alt", data[i].title);
-        let figcaption = document.createElement("figcaption");
-        figcaption.textContent = data[i].title;
-        figure.appendChild(image);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-    }})
