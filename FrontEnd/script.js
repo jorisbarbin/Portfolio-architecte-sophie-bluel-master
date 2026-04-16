fd222ee6016b04b602d331db61df622640909d47
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-16 13:52:08 +0200

    filters categorys ok

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,7 +3,7 @@ gallery.innerHTML = "";
 const filters = document.querySelector(".filters");
 filters.innerHTML = "";
 let dataWorks = [];
-
+let dataFilters = [];
 
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
@@ -23,40 +23,34 @@ fetch("http://localhost:5678/api/works")
         figure.appendChild(image);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
+
+        
     }}
-
-
-
 
 fetch("http://localhost:5678/api/categories")
     .then((response) => response.json())
     .then((data) => {
         dataFilters = data;
-
         let buttonAll = document.createElement("button");
         buttonAll.textContent = "Tous";
         buttonAll.setAttribute("data-category-id", "all");
         filters.appendChild(buttonAll);
+
         for (let i = 0; i < dataFilters.length; i++) {
             let button = document.createElement("button");
             button.textContent = dataFilters[i].name;
             button.setAttribute("data-category-id", dataFilters[i].id);
             filters.appendChild(button);
-            button.addEventListener("click", () => {
-            console.log(button.getAttribute("data-category-id"))}
-            )
-        }
+
+        button.addEventListener("click", () => {
+            let categoryId = button.getAttribute("data-category-id");
+            AfficherGallery(dataWorks.filter(work => work.categoryId == categoryId));
+        console.log(button.getAttribute("data-category-id"))}
+            )}
+
         buttonAll.addEventListener("click", () => {
-            
             AfficherGallery(dataWorks);
-            
-            console.log(buttonAll.getAttribute("data-category-id"))
-            console.log(gallery.innerHTML)
-            console.log(AfficherGallery)
-        }
-            
-            )
+        })})
 
-        console.log("Categories ok:", dataWorks);
 
-    })
+
