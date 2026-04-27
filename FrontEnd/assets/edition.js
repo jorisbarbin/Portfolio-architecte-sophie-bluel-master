d3c283e36d831436a5eb1bc7227a67405cb8b01c
Joris Barbin <barbin.joris@hotmail.fr>
2026-04-27 10:24:22 +0200

    Javascript cutting separate files

---
+++ FrontEnd/assets/edition.js
@@ -0,0 +1,46 @@
+const loginLink = document.querySelector("#login-link");
+const EditionMode = document.querySelector("#EditionMode");
+EditionMode.style.display = "none";
+const ModifierBlock = document.querySelector(".modifier-block")
+ModifierBlock.style.display = "none";
+const ModaleEdition = document.querySelector("#ModaleEdition")
+ModaleEdition.style.display = "none";
+const Xmark = document.querySelector("#Xmark");
+const Overlay = document.querySelector("#Overlay")
+Overlay.style.display = "none";
+
+fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+        dataFilters = data;
+        filters.innerHTML = "";
+        if (sessionStorage.getItem("token")) {
+            loginLink.textContent = "Logout";
+            EditionMode.textContent = "Mode édition";
+            EditionMode.style.display = "block";
+            ModifierBlock.style.display = "flex";
+            loginLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            sessionStorage.removeItem("token");
+            window.location.reload();
+        })
+        ModifierBlock.addEventListener("click", (event) => {
+            event.preventDefault()
+            ModaleEdition.style.display = "block";
+            Overlay.style.display = "block";
+        })
+        Xmark.addEventListener("click", (event) => {
+            event.preventDefault()
+            ModaleEdition.style.display = "none";
+            Overlay.style.display = "none";
+        })
+        Overlay.addEventListener("click", (event) => {
+            event.preventDefault()
+            ModaleEdition.style.display = "none";
+            Overlay.style.display = "none";
+        })
+
+        
+        } else {
+            AfficherFilters(dataFilters);
+    }})
---
+++ FrontEnd/assets/gallery.js
@@ -0,0 +1,50 @@
+let gallery = document.querySelector(".gallery");
+gallery.innerHTML = "";
+const filters = document.querySelector(".filters");
+filters.innerHTML = "";
+
+let dataWorks = [];
+let dataFilters = [];
+
+fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((data) => {
+        dataWorks = data;
+        AfficherGallery(dataWorks);
+    })
+        function AfficherGallery(dataWorks) {
+        gallery.innerHTML = "";
+        for (let i = 0; i < dataWorks.length; i++) {
+        let figure = document.createElement("figure");
+        let image = document.createElement("img");
+        image.setAttribute("src", dataWorks[i].imageUrl);
+        image.setAttribute("alt", dataWorks[i].title);
+        let figcaption = document.createElement("figcaption");
+        figcaption.textContent = dataWorks[i].title;
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }}
+
+        function AfficherFilters(dataFilters) {
+        let buttonAll = document.createElement("button");
+        buttonAll.textContent = "Tous";
+        buttonAll.setAttribute("data-category-id", "all");
+        filters.appendChild(buttonAll);
+
+        for (let i = 0; i < dataFilters.length; i++) {
+            let button = document.createElement("button");
+            button.textContent = dataFilters[i].name;
+            button.setAttribute("data-category-id", dataFilters[i].id);
+            filters.appendChild(button);
+
+        button.addEventListener("click", () => {
+            let categoryId = button.getAttribute("data-category-id");
+            AfficherGallery(dataWorks.filter(work => work.categoryId == categoryId));})
+        }
+
+
+        buttonAll.addEventListener("click", () => {
+            AfficherGallery(dataWorks);
+        })
+    }
